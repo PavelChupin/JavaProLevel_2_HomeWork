aeba5bc058aa6cee5e18d24e91b92a396fe8ac00
Chupin Pevel <pavel.chupin@gmail.com>
2019-11-05 14:46:59 +0700

    Создание репозитория домашних работ

---
+++ src/main/java/homework/lesson2/javafx/controller/message/ServerConnectionException.java
@@ -0,0 +1,8 @@
+package homework.lesson2.javafx.controller.message;
+
+public class ServerConnectionException extends RuntimeException {
+
+    public ServerConnectionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
