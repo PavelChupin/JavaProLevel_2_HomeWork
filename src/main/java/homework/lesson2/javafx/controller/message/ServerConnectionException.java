cbf6cbffa888e46ef6d034766bae815f11560800
Chupin Pevel <pavel.chupin@gmail.com>
2019-11-17 14:46:10 +0700

    Первичная реализация

--- src/main/java/homework/lesson2/javafx/controller/message/ServerConnectionException.java
+++
@@ -1,8 +0,0 @@
-package homework.lesson2.javafx.controller.message;
-
-public class ServerConnectionException extends RuntimeException {
-
-    public ServerConnectionException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
