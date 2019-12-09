b6a4b12c8780b796027ba4fb6358a38237529b89
Chupin Pevel <pavel.chupin@gmail.com>
2019-12-09 15:45:23 +0700

    Реализовано ДЗ8. Сразу скажу немного помог коллега, подкинул идею как обходить массив по сперале. Я сначало хотел его как то обойти и какуюто формулу закономерность выделить, но так получилось проще.

---
+++ src/main/java/homework/lesson8/Main.java
@@ -0,0 +1,72 @@
+package homework.lesson8;
+
+public class Main {
+    public static final int X = 1;
+    public static final int Y = 3;
+
+    public static void main(String[] args) {
+        new ArrOutput().outPutArr(2, 3);
+        new ArrOutput().outPutArr(4, 4);
+        new ArrOutput().outPutArr(3, 1);
+        new ArrOutput().outPutArr(3, 3);
+    }
+}
+
+class ArrOutput {
+    public void outPutArr(int x, int y) {
+        int[][] arr = new int[x][y];
+        int c = 1;
+        int count = 1;
+        int y1 = 0, x1 = 0;
+
+        arr[0][0] = c;
+        for (int t = 0; t < x * y; t++) {
+            if (count == 1) {
+                if (x1 == x - 1 || arr[x1 + 1][y1] != 0) {
+                    count = 2;
+                } else {
+                    x1++;c++;
+                }
+            }
+
+            if (count == 2) {
+                if (y1 == y - 1 || arr[x1][y1 + 1] != 0) {
+                    count = 3;
+                } else {
+                    y1++;c++;
+                }
+            }
+
+            if (count == 3) {
+                if (x1 == 0 || arr[x1 - 1][y1] != 0) {
+                    count = 4;
+                } else {
+                    x1--;c++;
+                }
+            }
+
+            if (count == 4) {
+                if (y1 == 0 || arr[x1][y1 - 1] != 0) {
+                    count = 1;
+                } else {
+                    y1--;c++;
+                }
+            }
+
+            arr[x1][y1] = c;
+        }
+        print(arr);
+        System.out.println();
+
+
+    }
+
+    private void print(int[][] arr) {
+        for (int i = 0; i < arr[0].length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[j][i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
